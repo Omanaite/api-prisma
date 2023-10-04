13c0547bdf6b10b0f0399d3dcb6f1663454b7178
Omanaite <pchandia@hotmail.com>
2023-10-04 15:16:23 +0200

    1st commit

---
+++ index.ts
@@ -0,0 +1,98 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    // ... you will write your Prisma Client queries here
+    //adding records
+    // await prisma.user.create({
+
+    //     data: {
+
+    //         name: 'Rich',
+
+    //         email: 'hello@prisma.com',
+
+    //         posts: {
+
+    //             create: {
+
+    //                 title: 'My first post',
+
+    //                 body: 'Lots of really interesting stuff',
+
+    //                 slug: 'my-first-post',
+
+    //             },
+
+    //         },
+
+    //     },
+
+    // })
+    //Adding Comments (updating record)
+    await prisma.post.update({
+
+        where: {
+    
+          slug: 'my-first-post',
+    
+        },
+    
+        data: {
+    
+          comments: {
+    
+            createMany: {
+    
+              data: [
+    
+                { comment: 'Great post!' },
+    
+                { comment: "Can't wait to read more!" },
+    
+              ],
+    
+            },
+    
+          },
+    
+        },
+    
+      })
+    
+      const posts = await prisma.post.findMany({
+    
+        include: {
+    
+          comments: true,
+    
+        },
+    
+      })
+    
+    
+      console.dir(posts, { depth: Infinity })
+
+
+    const allUsers = await prisma.user.findMany({
+
+        include: {
+
+            posts: true,
+
+        },
+
+    })
+
+    console.dir(allUsers, { depth: null })
+}
+
+main()
+    .catch(async (e) => {
+        console.error(e)
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
